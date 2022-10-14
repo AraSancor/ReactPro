030ac3c208fc12964c15fe1fbf6e08a50780cc15
AraSancor <88673841+AraSancor@users.noreply.github.com>
2022-10-14 12:42:01 +0200

    calc

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import fccLogo from './img/fcc.png';
+import Boton from './componentes/Boton';
 
 function App() {
   return (
-    <div className="App">
-      hola
+    <div className='App'>
+      <div className='fcc-logo-cont'>
+        <img
+          src={fccLogo}
+          className='fcc-logo'
+          alt='Logo' />
+      </div>
+
+      <div className='cont-calc'>
+        <div className='fila'>
+          <Boton>1</Boton>
+          <Boton>+</Boton>
+          <Boton>3</Boton>
+        </div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+      </div>
+
     </div>
   );
 }
