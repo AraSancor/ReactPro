8f56cd9bcf46042c123c4ed54afae3c96d24c9ce
AraSancor <88673841+AraSancor@users.noreply.github.com>
2022-10-18 18:46:37 +0200

    Ges

---
+++ gestor/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Logo from './img/fcc.png';
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+
+      <div className='logo-contenedor'>
+<img
+src={Logo} className='logo' />
+      </div>
+   
+   <div className='tareas-lista-principal'>
+<h1>Mis deberes</h1>
+
+   </div>
+    </div>
+  );
+}
+
+export default App;
